59ca74368cb19579d3e52f37bbc5576146f0c9ec
EndyKaufman <admin@site15.ru>
2024-01-09 10:57:32 +0400

    fix: share all used files

--- libs/system/nestjs-pino/src/index.ts
+++ libs/system/nestjs-pino/src/index.ts
@@ -1,1 +1,4 @@
+export * from './lib/logger-request-id.interceptor';
+export * from './lib/nestjs-pino.async-local-storage';
+export * from './lib/nestjs-pino.configuration';
 export * from './lib/nestjs-pino.module';
