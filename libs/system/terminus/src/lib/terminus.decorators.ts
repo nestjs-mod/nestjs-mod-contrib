9d85ae133950104a9cd686aa8a4c12784f615fc1
EndyKaufman <admin@site15.ru>
2024-01-18 11:15:51 +0400

    fix: add all decorators to terminus

--- libs/system/terminus/src/lib/terminus.decorators.ts
+++ libs/system/terminus/src/lib/terminus.decorators.ts
@@ -1,6 +1,12 @@
 import { getNestModuleDecorators } from '@nestjs-mod/common';
 import { TERMINUS_MODULE_NAME } from './terminus.constants';
 
-export const { InjectFeatures: InjectTerminusHealthCheckFeatures } = getNestModuleDecorators({
+export const {
+  InjectFeatures: InjectTerminusHealthCheckFeatures,
+  InjectAllFeatures: InjectAllTerminusHealthCheckFeatures,
+  InjectAllFeatureEnvironments: InjectTerminusAllHealthCheckFeatureEnvironments,
+  InjectFeatureEnvironments: InjectTerminusHealthCheckFeatureEnvironments,
+  InjectService: InjectTerminusHealthCheckService,
+} = getNestModuleDecorators({
   moduleName: TERMINUS_MODULE_NAME,
 });
