612fdf6cce3bbd624e7b44cc239b24faf794e1a5
EndyKaufman <admin@site15.ru>
2024-02-13 20:02:30 +0400

    chore: add pingDatabaseIntervalMs to prisma config for disable setInterval

--- apps/example-graphql/src/app/services/balance-of-user.service.ts
+++ apps/example-graphql/src/app/services/balance-of-user.service.ts
@@ -2,20 +2,22 @@ import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { randomInt } from 'crypto';
 import { PubSub } from 'graphql-subscriptions';
 import { CHANGE_USER_BALANCE_EVENT, UserBalanceDto } from '../resolvers/dto/user-balance.dto';
+import { isInfrastructureMode } from '@nestjs-mod/common';
 
 @Injectable()
 export class BalanceOfUserService extends PubSub implements OnModuleInit, OnModuleDestroy {
   changeBalanceIntervalRef?: NodeJS.Timer;
 
   onModuleInit() {
-    this.changeBalanceIntervalRef = setInterval(
-      () => this.publish(CHANGE_USER_BALANCE_EVENT, { amount: randomInt(1000, 9999), userId: 2 } as UserBalanceDto),
-      3000
-    );
+    if (!isInfrastructureMode()) {
+      this.changeBalanceIntervalRef = setInterval(() => {
+        this.publish(CHANGE_USER_BALANCE_EVENT, { amount: randomInt(1000, 9999), userId: 2 } as UserBalanceDto).then();
+      }, 3000);
+    }
   }
 
   onModuleDestroy() {
-    if (this.changeBalanceIntervalRef){
+    if (this.changeBalanceIntervalRef) {
       clearInterval(this.changeBalanceIntervalRef);
     }
   }
