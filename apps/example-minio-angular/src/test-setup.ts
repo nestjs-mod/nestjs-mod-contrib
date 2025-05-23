0cf6770d2044a9de695eb0dd93cde57a660d1b3d
EndyKaufman <admin@site15.ru>
2025-05-23 19:44:14 +0500

    chore: try update test-setup

--- apps/example-minio-angular/src/test-setup.ts
+++ apps/example-minio-angular/src/test-setup.ts
@@ -1,7 +1,3 @@
-globalThis.ngJest = {
-  testEnvironmentOptions: {
-    errorOnUnknownElements: true,
-    errorOnUnknownProperties: true,
-  },
-};
-import 'jest-preset-angular/setup-jest';
+import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
+
+setupZoneTestEnv();
